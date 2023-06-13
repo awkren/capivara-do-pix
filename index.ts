7241a802a33644696292541f2fe07129be367973
Renan Araújo <awkrenan@gmail.com>
2023-06-12 22:50:23 -0300

    setup sqlite

--- index.ts
+++ index.ts
@@ -1,13 +1,17 @@
 import express, { Express, Request, Response } from "express";
 import dotenv from "dotenv";
+import initializeDatabase from "./db";
 
 dotenv.config();
 
 const app: Express = express();
 const port = process.env.PORT;
 
+// init database
+initializeDatabase();
+
 app.get("/", (req: Request, res: Response) => {
-  res.send("pixramide");
+  res.send("oi");
 });
 
 app.listen(port, () => {
