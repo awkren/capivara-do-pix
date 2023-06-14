26b245479c6116cc330c00a650e8b8c6bf51b380
Renan Araújo <awkrenan@gmail.com>
2023-06-13 21:37:57 -0300

    added profit per day

--- index.ts
+++ index.ts
@@ -1,6 +1,7 @@
 import express, { Request, Response } from "express";
 import dotenv from "dotenv";
 import mysql from "mysql";
+import schedule from "node-schedule";
 
 dotenv.config();
 
@@ -24,6 +25,18 @@ app.get("/", (req: Request, res: Response) => {
   res.send("oi");
 });
 
+app.get("/users", (req: Request, res: Response) => {
+  // retrieve users from db
+  pool.query("SELECT * FROM users", (error, results) => {
+    if (error) {
+      console.error("Error retrieving users: ", error);
+      res.status(500).send("Error retrieving users");
+    } else {
+      res.json(results);
+    }
+  });
+});
+
 app.post("/users", (req: Request, res: Response) => {
   const { id, name, investment } = req.body;
 
@@ -41,6 +54,39 @@ app.post("/users", (req: Request, res: Response) => {
     }
   });
 });
+
+// calculate profit
+const calculateProfits = () => {
+  // retrieve users from db
+  pool.query("SELECT * FROM users", (error, results) => {
+    if (error) {
+      console.error("Error retrieving users", error);
+    } else {
+      // iterate through users and calculate profit
+      results.forEach((user: any) => {
+        const investment = user.investment;
+        const valueProfit = user.profit;
+        const profit = valueProfit + investment * 0.3333;
+
+        // update user's profit in the database
+        pool.query(
+          "UPDATE users SET profit = ? WHERE id = ?",
+          [profit, user.id],
+          (error) => {
+            if (error) {
+              console.error("Error updating user profite: ", error);
+            } else {
+              console.log("Profit calculated and updated for user: ", user.id);
+            }
+          }
+        );
+      });
+    }
+  });
+};
+
+// schedule task to run every minute
+schedule.scheduleJob("* * * * *", calculateProfits);
 
 app.listen(port, () => {
   console.log(`Running on port ${port}`);
